f3e9855de2f7b4f869dc9083121cf26a67c09f79
Vasiliy Rodin <rodinvasiliy@gmail.com>
2025-08-19 11:07:42 -0700

    create footer and span for view counter create counter js         target the span         use a nullish coalescing to set default to 0 if null 	use a ternary operator (if/else) to check if current is Finite add 1 else set it to 1 	set local storage to next 	show next in span

---
+++ resume/counter.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    let ele = document.getElementById('visit_count');
+    let current = localStorage.getItem('visit_counter') ?? 0; // Nullish coalescing
+/*     if(Number(isFinite(current)){
+        next = Number(current)+ 1;
+    }else{
+        next = 1;
+    } */
+    let next = Number(isFinite(current)) ? Number(current) + 1: 1; //ternary operator. if/else
+    localStorage.setItem('visit_counter',String(next));
+    ele.textContent = String(next);
+})
