52d19ae085bff0852e1231839c70cc04c915ba6e
Vasiliy Rodin <rodinvasiliy@gmail.com>
2025-08-26 13:18:10 -0700

    fix response url from \ to/

--- resume/counterAPI/counter.js
+++ resume/counterAPI/counter.js
@@ -4,7 +4,7 @@
         if (!element) return;
         const base = 'https://xr5fwzutcc.execute-api.us-east-1.amazonaws.com/prod';
         const seen = localStorage.getItem("vr-site-seen")// on the first run through this is false.
-        const response = await fetch(`${base}\hit`, seen //fetch takes the url(required) and an option variable which here is being determined but the if seen is true
+        const response = await fetch(`${base}/hit`, seen //fetch takes the url(required) and an option variable which here is being determined but the if seen is true
             ? {method: "GET"}
             : {
                 method: "POST",
